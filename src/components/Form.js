2cd2974c2e2de0b309ba0dd1875fe830c092c1ae
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-04-19 05:48:30 -0500

    Add handleClick method -- no functionality yet

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import InputList from './InputList';
+// import InputList from './InputList';
 import InputItem from './InputItem';
 import Button from './Button';
 
 class Form extends Component {
-        render() {
-            // console.log(this.props);
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-            let inputItems = [];
-            this.props.formLabels.forEach((label, index) => {
-                let itemKey = this.props.formType + index;
-                if (label === 'Responsibilities' || label === 'Activities and Awards') {
-                    inputItems.push(<InputList key={itemKey} id={itemKey} label={label}/>);
-                } else {
-                    inputItems.push(<InputItem key={itemKey} id={itemKey} label={label}/>)
-                }
-            });
+    handleClick() {
+        console.log(this.props);
+        console.log(this.state);
+    }
 
-            let removeBtn;
-            if (this.props.formType !== 'p') {
-                removeBtn = <Button do='removeForm' buttonContent='Remove'/>
+    render() {
+        // console.log(this.props);
+
+        let inputItems = [];
+        this.props.formLabels.forEach((label, index) => {
+            let itemKey = this.props.formType + index;
+            if (label === 'Responsibilities' || label === 'Activities and Awards') {
+                // inputItems.push(<InputList key={itemKey} id={itemKey} label={label}/>);
+            } else {
+                inputItems.push(<InputItem key={itemKey} id={itemKey} label={label}/>)
             }
+        });
 
-            return (
-                <>
-                    {inputItems}
-                    {removeBtn}
-                </>
-            )
+        let removeBtn;
+        if (this.props.formType !== 'p') {
+            removeBtn = <Button do='removeForm' handleClick={this.handleClick} buttonContent='Remove'/>
         }
+
+        return (
+            <>
+                {inputItems}
+                {removeBtn}
+            </>
+        )
+    }
 }
 Form.propTypes = {
     formLabels: PropTypes.array.isRequired,
