82a2245a58783b917ce1147d5b11aa20e0b0e56d
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-04-22 11:34:59 -0500

    Create empty addItem/removeItem methods

--- src/components/InputList.js
+++ src/components/InputList.js
@@ -3,13 +3,31 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 class InputList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+        }
+
+        this.addItem = this.addItem.bind(this);
+        this.removeItem = this.removeItem.bind(this);
+    }
+
+    addItem() {
+        console.log('add item with unique id');
+    }
+
+    removeItem() {
+        console.log('remove item by unique id');
+    }
+    
     render() {
         // style with grid
         return (
             <div>
                 <label htmlFor={this.props.id}>{this.props.label}:</label>
-                <Button do='addListItem' buttonContent='+'/>
-                <Button do='removeListItem' buttonContent='-'/>
+                <Button handleClick={this.addItem} buttonContent='+'/>
+                <Button handleClick={this.removeItem} buttonContent='-'/>
                 <div className='item-container'></div>
             </div>
         )
