97c875405e8c3b9177cab75108f1e760a6baef1f
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-04-22 11:39:51 -0500

    Change uniquid import to use single-quotes for consistency

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import uniqid from "uniqid";
+import uniqid from 'uniqid';
 import Form from './Form';
 import Button from './Button';
 
