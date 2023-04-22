6e07e780f92bcae4c6dfb844630a6bf808a55c03
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-04-22 12:30:35 -0500

    Add items to input list

--- src/components/InputList.js
+++ src/components/InputList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import uniqid from 'uniqid';
+import Input from './Input';
 import Button from './Button';
 
 class InputList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [],
+            list: [],
         }
 
         this.addItem = this.addItem.bind(this);
@@ -14,7 +16,11 @@ class InputList extends Component {
     }
 
     addItem() {
-        console.log('add item with unique id');
+        let listCopy = this.state.list;
+        let newID = uniqid();
+        listCopy.push({id: newID, value: ''});
+        this.setState({items: listCopy});
+        console.log(this.state.list);
     }
 
     removeItem() {
@@ -28,7 +34,11 @@ class InputList extends Component {
                 <label htmlFor={this.props.id}>{this.props.label}:</label>
                 <Button handleClick={this.addItem} buttonContent='+'/>
                 <Button handleClick={this.removeItem} buttonContent='-'/>
-                <div className='item-container'></div>
+                <div className='item-container'>
+                    {this.state.list.map(object => (
+                        <Input key={object.id} inputType='text' id={object.id} value={object.value}/>
+                    ))}
+                </div>
             </div>
         )
     }
