476ec9a9d1b7043c7f15040090027e10279623e1
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-04-25 06:14:40 -0500

    Refactor InputList method to correctly alter state

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 class Input extends Component {
     render() {
         // console.log(this.props);
+        // todo make id descriptive, use data-inputKey for state queries
         
         return (
-            <input type={this.props.inputType} id={this.props.id} onFocus={this.props.handleFocus} onChange={this.props.handleChange}></input>
+            <input key={this.props.inputKey} data-input-key={this.props.inputKey} type={this.props.inputType} id={this.props.id} onFocus={this.props.handleFocus} onChange={this.props.handleChange}></input>
         )
     }
 }
 Input.propTypes = {
-    inputType: PropTypes.string.isRequired,
+    inputKey: PropTypes.string,
+    inputType: PropTypes.string,
     id: PropTypes.string.isRequired,
     handleFocus: PropTypes.func,
     handleChange: PropTypes.func,
--- src/components/InputList.js
+++ src/components/InputList.js
@@ -4,6 +4,8 @@ import uniqid from 'uniqid';
 import Input from './Input';
 import Button from './Button';
 
+// todo refactor methods to reduce state copies and redundant code
+
 class InputList extends Component {
     constructor(props) {
         super(props);
@@ -15,53 +17,46 @@ class InputList extends Component {
         this.addItem = this.addItem.bind(this);
         this.removeItem = this.removeItem.bind(this);
         this.handleFocus = this.handleFocus.bind(this);
-        this.handleChange = this.handleChange.bind(this);
     }
 
     addItem() {
-        let listCopy = this.state.list;
         let newID = uniqid();
-        listCopy.push({id: newID, value: ''});
-        this.setState({list: listCopy});
+        let inputKey = `${this.props.listID}-${newID}`;
+        this.setState({list: [...this.state.list, {id: inputKey, input: <Input key={inputKey} inputKey={inputKey} id={inputKey} handleFocus={this.handleFocus} handleChange={this.handleChange} inputType='text'/>}]});
     }
 
     removeItem() {
-        let listCopy = this.state.list;
-        listCopy.splice(this.state.currentTarget, 1);
-        this.setState({list: listCopy});
-        this.setState({currentTarget: null});
+        this.setState({list: this.state.list.filter(item =>
+            item.id !== this.state.currentTarget
+        )});
     }
 
     handleFocus(e) {
-        let listCopy = this.state.list;
-        let targetIndex = listCopy.findIndex(object => object.id === e.target.id);
-        this.setState({currentTarget: targetIndex});
-    }
-
-    handleChange(e) {
-        let listCopy = this.state.list;
-        listCopy[this.state.currentTarget].value = e.target.value;
-        this.setState({list: listCopy});
+        this.setState({currentTarget: e.target.dataset.inputKey});
     }
     
     render() {
         // style with grid
+        let inputItems = [];
+        this.state.list.forEach(item => {
+            inputItems.push(item.input);
+        })
+        console.log(inputItems);
+
         return (
             <div>
-                <label htmlFor={this.props.id}>{this.props.label}:</label>
+                <label htmlFor={this.props.listID}>{this.props.label}:</label>
                 <Button handleClick={this.addItem} buttonContent='+'/>
                 <Button handleClick={this.removeItem} buttonContent='-'/>
                 <div className='item-container'>
-                    {this.state.list.map(object => (
-                        <Input key={object.id} handleFocus={this.handleFocus} handleChange={this.handleChange} inputType='text' id={object.id} value={object.value}/>
-                    ))}
+                    {inputItems}
                 </div>
             </div>
         )
     }
 }
 InputList.propTypes = {
-    id: PropTypes.string.isRequired,
+    listID: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired
 }
 
