cb29f277e1741d893c7ca2d06ead789d864177d4
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-05-05 05:47:34 -0500

    Create components to display form data

---
+++ src/components/Display.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import uniqid from 'uniqid';
+
+class Display extends Component {
+    render() {
+        // todo display without 'name' 'website' etc. -- just one-by-one in an array
+        let displayValues = [];
+        if (this.props.displayContent !== undefined) {
+            for (let i = 0; i < this.props.displayContent.formItems.length; i++) {
+                displayValues.push(<span>{this.props.displayContent.formItems[i].itemValue}</span>);
+            }
+        }
+        return (
+            <div>
+                {displayValues} 
+            </div>
+        )
+    }
+}
+
+Display.propTypes = {
+    displayContent: PropTypes.object,
+};
+
+export default Display;
--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import uniqid from 'uniqid';
+import Display from './Display';
 
 class DisplaySection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mounted: false,
+        }
+    }
+
+    componentDidMount() {
+        this.setState({mounted: true});
+    }
+
     render() {
+        let sectionTitle;
+        if (this.props.sectionType === 'w' && this.props.displayContent.length > 0) {
+            sectionTitle = <h2>Work Experience</h2>;
+        } else if (this.props.sectionType === 'e' && this.props.displayContent.length > 0) {
+            sectionTitle = <h2>Education Experience</h2>;
+        }
+
+        let displayElements = [];
+        if (this.state.mounted) {
+            this.props.displayContent.forEach(formContent => {
+                displayElements.push(<Display key={uniqid()} displayContent={formContent}/>);
+            });
+        }
+        
         return (
-            console.log('DisplaySection')
+            <section className={this.props.sectionType}>
+                {sectionTitle}
+                {displayElements}
+            </section>
         )
     }
+}
+DisplaySection.propTypes = {
+    sectionType: PropTypes.string,
+    displayContent: PropTypes.array,
 }
 
 export default DisplaySection;
