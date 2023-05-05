1fe528a285011ea7d69c7b28e3dc5ffa5f7a4557
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-05-05 05:48:56 -0500

    Add uniqe ids to <span> jsx elements

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import uniqid from 'uniqid';
+import uniqid from 'uniqid';
 
 class Display extends Component {
     render() {
@@ -8,7 +8,7 @@ class Display extends Component {
         let displayValues = [];
         if (this.props.displayContent !== undefined) {
             for (let i = 0; i < this.props.displayContent.formItems.length; i++) {
-                displayValues.push(<span>{this.props.displayContent.formItems[i].itemValue}</span>);
+                displayValues.push(<span key={uniqid()}>{this.props.displayContent.formItems[i].itemValue}</span>);
             }
         }
         return (
