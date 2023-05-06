c35f4721ab3eecaba814cbccaf7421311925f934
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-05-06 09:13:32 -0500

    Position work display

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,9 +5,21 @@ import uniqid from 'uniqid';
 class Display extends Component {
     render() {
         let displayValues = [];
+        let dateValues = [];
         if (this.props.displayContent !== undefined) {
             for (let i = 0; i < this.props.displayContent.formItems.length; i++) {
-                displayValues.push(<span key={uniqid()}>{this.props.displayContent.formItems[i].itemValue}</span>);
+                if (this.props.displayContent.formItems[i].itemID.includes('date') && this.props.displayContent.formItems[i].itemValue !== '') {
+                    dateValues.push(this.props.displayContent.formItems[i].itemValue);
+                } else if (!this.props.displayContent.formItems[i].itemID.includes('date')) {
+                    displayValues.push(<span key={uniqid()} id={this.props.displayContent.formItems[i].itemID}>{this.props.displayContent.formItems[i].itemValue}</span>);
+                }
+            }
+            if (this.props.displayType !== 'p') {
+                if (dateValues.length > 0) {
+                    displayValues.push(<span key={uniqid()} id='date'>{dateValues.join(' - ')}</span>);
+                } else {
+                    displayValues.push(<span key={uniqid()} id='date'>{''}</span>);
+                }
             }
         }
         return (
